8357f19b456fee553496d973cef7b153d2ae1b31
梁皓铭 <lianghaoming@example.com>
2026-03-14 11:55:46 +0800

    docs: 更新部署架构文档，移除cloudbaseignore

--- functions/magic-rules-api/deploy_package/backend/services/__init__.py
+++
@@ -1,6 +0,0 @@
-from .rule_service import RuleService
-from .rule_downloader import RuleDownloader
-from .card_service import CardService
-from .card_downloader import CardDownloader
-
-__all__ = ["RuleService", "RuleDownloader", "CardService", "CardDownloader"]
